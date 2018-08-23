e3e0da2215d0078002b9f1700117aa3feac45dfd
Ricardo Gatica M <hola@ricardogatica.com>
2018-08-23 16:09:22 -0300

    chore(amd): Se agrega trabajo para calcular precio de venta de procesadores.
    El programa pide el valor del procesdador y la cantidad que se venderá.
    
    La condición para aplicar descuento al total de la venta es la siguiente:
    
    - hasta 9 unidades tiene 8%
    - hasta 19 unidades tiene 18%
    - hasta 30 unidades tiene un 28%
    - más de 30 unidades tiene un 35%
    
    Se calcula el 19% y muestra el total a pagar

--- P2AMD_Gatica.cpp
+++ P2AMD_Gatica.cpp
@@ -1,1 +1,48 @@
-P2AMD_Gatica.cpp
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int descuento = 0, iva = 19, valor_procesador = 0, procesadores;
+
+	long long total = 0, total_iva = 0, total_pago = 0;
+
+	// Ingreso de datos
+	cout<<"\n Ingrese valor del procesador: ";
+	cin>>valor_procesador;
+
+	cout<<"\n Ingrese cantidad de procesadores: ";
+	cin>>procesadores;
+
+	if (procesadores > 30) {
+		descuento = 35;
+	}
+	else if (procesadores >= 20 && procesadores <= 30) {
+		descuento = 28;
+	}
+	else if (procesadores >= 10 && procesadores < 20) {
+		descuento = 18;
+	}
+	else {
+		descuento = 8;
+	}
+
+	total = ((procesadores * valor_procesador) - ((descuento * (procesadores * valor_procesador)) / 100));
+	total_iva = ((iva * total) / 100);
+	total_pago = total + total_iva;
+
+	cout<<"\n Procesadores: "<<procesadores;
+	cout<<"\n Total: $"<<total;
+	cout<<"\n Iva: $"<<total_iva;
+
+	cout<<"\n Total + Iva: $"<<total_pago;
+
+	cout<<endl;
+	return 0;
+}
---
+++ SegundosHoras.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+// #include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+	// Definición de variable
+	int segundos = 0, minutos = 0, horas = 0;
+
+
+	int MINUTO = 60; // Segundos de un minuto
+	int HORA = MINUTO * 60; // Segundos de una hora
+	
+
+	// Ingreso de datos
+	cout<<"\n Ingrese cantidad de segundos: ";
+
+	cin>>segundos;
+
+	horas = segundos / HORA;
+	minutos = (segundos - (horas * HORA)) / MINUTO;
+	segundos = segundos - (horas * HORA) - (minutos * MINUTO);
+
+	cout<<"\n Esa cantidad corresponde a "<<horas<<" horas, "<<minutos<<" minutos y "<<segundos<<" segundos.";
+
+	cout<<endl;
+
+	return 0; // Devuelve control al sistema
+}
